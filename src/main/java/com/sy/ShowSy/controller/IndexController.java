1f41c345414e384597bceeb726f820d286420712
albert <790321193@qq.com>
2018-01-09 17:04:19 +0800

    ---

--- src/main/java/com/sy/ShowSy/controller/IndexController.java
+++ src/main/java/com/sy/ShowSy/controller/IndexController.java
@@ -3,6 +3,9 @@
  */
 package com.sy.ShowSy.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /** 
 * @ClassName: IndexController 
 * @Description: 
@@ -10,9 +13,12 @@ package com.sy.ShowSy.controller;
 * @date 2017年11月3日 下午4:19:50 
 *  
 */
+@RestController
+@RequestMapping("/")
 public class IndexController {
+	@RequestMapping(value={"","/index"})
 	public String index(){
 		System.out.println("hahah fdsafdsa");
-		return "index";
+		return "indexfuck";
 	}
 }
